3fc6647b177db2e881cf0533a25199fbd8a560d3
Rigneswari A P Visuvanathan <18DDT19F1078@student.ptss.edu.my>
2021-04-17 18:53:26 +0800

    Rigneswari

---
+++ Array/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+            Dim strNamaHari(6) As String
+            Dim a As Integer
+
+            strNamaHari(0) = "Ahad"
+            strNamaHari(1) = "Isnin"
+            strNamaHari(2) = "Selasa"
+            strNamaHari(3) = "Rabu"
+            strNamaHari(4) = "Khamis"
+            strNamaHari(5) = "Jumaat"
+            strNamaHari(6) = "Sabtu"
+
+        System.Array.Reverse(strNamaHari)
+        For a = 0 To 6
+                ListBox1.Items.Add(strNamaHari(a))
+            Next
+        End Sub
+
+
+    End Class
+
